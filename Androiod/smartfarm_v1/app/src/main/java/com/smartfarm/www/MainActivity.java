264bcb470130df191590602175d8679b8893d2b6
shinwonjin <https://github.com/labory4302/SmartFarm.git>
2020-09-09 15:26:51 +0900

    0909_1525_SWJ
    android basic frame

---
+++ Androiod/smartfarm_v1/app/src/main/java/com/smartfarm/www/MainActivity.java
@@ -0,0 +1,22 @@
+package com.smartfarm.www;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        new GetWeatherTask().execute();
+
+    }
+
+
+}
