a4fe63e4dc7fcad5f4a4df8e2877efb8dd27f55a
chan <git@github.com:chan-kim961111/SmartFarm.git>
2020-09-24 17:13:44 +0900

    0924_1712_KBC
    deeplearning_version_01 - error modify

--- Deeplearning/Crop_classification/result_version_v1.0.py
+++ Deeplearning/Crop_classification/result_version_v1.0.py
@@ -7,13 +7,13 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 # 0_2. 전역변수 및 상대 경로 지정
 #--------------------------------------------------------------
-roadimg_folder_src = 'C:/Users/USER/Desktop/HotSix/deeplearning/result_testimg_01'
-roadimg_src= 'C:/Users/USER/Desktop/HotSix/deeplearning/result_testimg_01/img/'
-roadimg_name = 'testimg_ca.jpg'
+roadimg_folder_src = './result_testimg_01'
+roadimg_src= './result_testimg_01/img/'
+roadimg_name = 'resultimg_01.jpg'
 
 vegetable_classnumber = 0
 result_disease = 0
-
+vegetable_class = ["배추", "고추", "깨"]
 result_class = []
 
 
@@ -53,7 +53,7 @@ model_pepper = load_model('test_pepper_class_01.h5')
 model_sesame = load_model('test_sesame_class_01.h5')
 
 # 각 작물에 대한 질병 학습 모델을 배열로 저장
-vegetable_model_class = []
+vegetable_model_class = ['', '', '']
 vegetable_model_class[0] = load_model('test_cabbage_class_01.h5')
 vegetable_model_class[1] = load_model('test_pepper_class_01.h5')
 vegetable_model_class[2] = load_model('test_sesame_class_01.h5')
@@ -61,59 +61,34 @@ vegetable_model_class[2] = load_model('test_sesame_class_01.h5')
 # 작물 종류에 대한 학습 모델에게 이미지 판단(0:배추, 1:고추, 2:깨)
 output = model_vegetable.predict_generator(test_generator)
 
-# 학습 모델이 판단한 이미지의 판단 결과 확률 임시 저장 변수
-#learning_pre_result = 0
-
-
 # 작물 종류 분류
 vegetable_classnumber = find_index(output)
-'''
-for i in range(len(output[0])):
-    if learning_pre_result < output[0][i]:
-        learning_pre_result = output[0][i]
-        vegetable_classnumber = i
-'''
 
 # 0번 인덱스인 배추의 질병 종류 판단
 if vegetable_classnumber == 0:
     
     result_output = vegetable_model_class[0].predict_generator(test_generator)
-    vegetable_classnumber = find_index(result_output)
+    result_disease = find_index(result_output)
     result_class = ["뿌리 혹병", "배추 역병", "무름병"]
-    '''
-    for i in range(len(output[0])):
-        if learning_pre_result < result_output[0][i]:
-            learning_pre_result = result_output[0][i]
-            vegetable_classnumber = i
-            '''
+
 # 1번 인덱스인 고추의 질병 종류 판단
 elif vegetable_classnumber == 1:
     result_output = vegetable_model_class[1].predict_generator(test_generator)
-    vegetable_classnumber = find_index(result_output)
+    result_disease = find_index(result_output)
     result_class = ["탄저병", "칼슘부족", "고추 역병"]
-    '''
-    for i in range(len(output[0])):
-        if learning_pre_result < result_output[0][i]:
-            learning_pre_result = result_output[0][i]
-            vegetable_classnumber = i
-            '''
+
 # 2번 인덱스인 깨의 질병 종류 판단
 elif vegetable_classnumber == 2:
     result_output = vegetable_model_class[2].predict_generator(test_generator)
-    vegetable_classnumber = find_index(result_output)
-    result_class = []
-    '''
-    for i in range(len(output[0])):
-        if learning_pre_result < result_output[0][i]:
-            learning_pre_result = result_output[0][i]
-            vegetable_classnumber = i
-     '''
+    result_disease = find_index(result_output)
+    result_class = ["마름병", "참깨 역병", "흰가루병"]
+
 # 해당 이미지 불러와 출력
 view_image = cv2.imread(roadimg_src + roadimg_name)
 cv2.imshow("result", view_image)
 
 # 학습 모델의 최종 병해 판단 결과 출력
-print("%s: %.2f%%" %(result_class[vegetable_classnumber], result_output[0][vegetable_classnumber]*100))
+print("작물은: %s, %s: %.2f%%" %(vegetable_class[vegetable_classnumber], result_class[result_disease], result_output[0][result_disease]*100))
 
 
     
