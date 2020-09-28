eca6b4e08b84765bc12cb3375d58e1a47be47a13
ssano <kssho3364@gmail.com>
2020-09-28 14:37:36 +0900

    0928_1437_KSH
    Add Crop Page, Crop Activity

---
+++ Androiod/smartfarm_v1/app/src/main/java/com/smartfarm/www/activity/CropActivity.java
@@ -0,0 +1,22 @@
+package com.smartfarm.www.activity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.smartfarm.www.R;
+
+public class CropActivity extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.crop_page, container, false);
+
+        return view;
+    }
+}
